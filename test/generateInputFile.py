ad8403b54d3bf1122aba355f26baadf56eec9a1d
Yifan Zhao <44952562+yifnzhao@users.noreply.github.com>
2019-01-30 14:37:04 -0500

    Add files via upload

---
+++ test/generateInputFile.py
@@ -0,0 +1,41 @@
+'''
+This module generates an input file for cas-offinder
+'''
+
+def parse(fileName):
+    """
+    this function parses the file and obtain the reads as a dictionary of {id:seq}
+    input: fileName (name of the file that you want to parse)
+    output: a dictionary whose keys are ids of reads and values are the correpsonding sequences    
+    """
+    from Bio import SeqIO
+    dictIDSeq={}
+    for record in SeqIO.parse(fileName, "fastq"):
+        print("%s %s" % (record, record.seq))
+        #print("%s" % (record.seq))
+        dictIDSeq[record.id]=record.seq
+    #print (dictIDSeq)
+    #for key, value in dictIDSeq.items():
+        #print(len(value)) about 250?
+    return dictIDSeq
+
+def dict2list(myDict):
+    """
+    this function converts a dicitonary of {id:seq} to a list of sequenced reads
+    """
+    return [value for key,value in myDict.items()]
+
+
+def writeInput(filename):
+    inputfilename='input_'+str(filename)+'.txt'
+    with open(inputfilename,'w') as f:
+        f.write('/Users/yifan/MiSeq/myreference/\n')
+        f.write('NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN\n')
+        mydictIDSeq = parse(filename)
+        myListSeq = dict2list(mydictIDSeq)
+        for item in myListSeq:
+            f.write("%s 100\n" % item)
+
+if __name__=="__main__":
+    writeInput("MI.M03555_0362.001.FLD0004.PTEN2-12_R1.fastq")
+    print('The input file has been created.')
