1dc90537433268c5f1d88916585556ea09917fc2
Yifan Zhao <44952562+yifnzhao@users.noreply.github.com>
2019-03-30 00:19:06 -0400

    Added sample #26

--- v2_inputValidator.py
+++ v2_inputValidator.py
@@ -78,7 +78,7 @@ def writeInput(filename, primer):
         for item in my100random_list_seq:
             f.write("%s\n" %item)
     
-#    
+    
 #    
 #    inputfilename=str(filename)+'_input_all.txt'
 #    with open(inputfilename,'w') as f:
@@ -92,53 +92,55 @@ def writeInput(filename, primer):
 #        print("\n")
 #        print("----------------------------------------------------------------------")
 #        print("The total number of reads in the input txt file below is: " + str(len(all_validated_seq)))    
-    
+#    
 
 if __name__=="__main__":
     
     
     
-#    stdoutOrigin=sys.stdout 
-#    sys.stdout = open("log20190301_all_validated_inputs.txt", "w")
+    stdoutOrigin=sys.stdout 
+    sys.stdout = open("log20190329_validated_inputs.txt", "w")
 
     PTEN2_primer='GTTGCACAGTATCCTTTTGAAGA'
     PTEN3_primer='AAGAAGTCCTTACATGGGTTGG'
     p53_primer='CTGTGCAGTTGTGGGTCAG' #new left primer
     BRCA1_primer='TTGTGAGCGTTTGAATGAGG'
-#    
-#    #p53
-#    os.chdir('/Users/yifan/MiSeq/data/MiSeq20190218/R1/p53/fastq')
-#    p53=[
-#'MI.M03992_0353.001.FLD0001.p53-1_R1.fastq',
-#'MI.M03992_0353.001.FLD0002.p53-13_R1.fastq',
-#'MI.M03992_0353.001.FLD0003.p53-25_R1.fastq',
-#'MI.M03992_0353.001.FLD0009.p53-2_R1.fastq',
-#'MI.M03992_0353.001.FLD0010.p53-14_R1.fastq',
-#'MI.M03992_0353.001.FLD0017.p53-3_R1.fastq',
-#'MI.M03992_0353.001.FLD0018.p53-15_R1.fastq',
-#'MI.M03992_0353.001.FLD0025.p53-4_R1.fastq',
-#'MI.M03992_0353.001.FLD0026.p53-16_R1.fastq',
-#'MI.M03992_0353.001.FLD0033.p53-5_R1.fastq',
-#'MI.M03992_0353.001.FLD0034.p53-17_R1.fastq',
-#'MI.M03992_0353.001.FLD0041.p53-6_R1.fastq',
-#'MI.M03992_0353.001.FLD0042.p53-18_R1.fastq',
-#'MI.M03992_0353.001.FLD0049.p53-7_R1.fastq',
-#'MI.M03992_0353.001.FLD0050.p53-19_R1.fastq',
-#'MI.M03992_0353.001.FLD0057.p53-8_R1.fastq',
-#'MI.M03992_0353.001.FLD0058.p53-20_R1.fastq',
-#'MI.M03992_0353.001.FLD0065.p53-9_R1.fastq',
-#'MI.M03992_0353.001.FLD0066.p53-21_R1.fastq',
-#'MI.M03992_0353.001.FLD0073.p53-10_R1.fastq',
-#'MI.M03992_0353.001.FLD0074.p53-22_R1.fastq',
-#'MI.M03992_0353.001.FLD0081.p53-11_R1.fastq',
-#'MI.M03992_0353.001.FLD0082.p53-23_R1.fastq',
-#'MI.M03992_0353.001.FLD0089.p53-12_R1.fastq',
-#'MI.M03992_0353.001.FLD0090.p53-24_R1.fastq'
-#]    
-#    for input in p53:
-#        writeInput(input,p53_primer)
-#        print('This input file has been created: m' + input+'_input.txt')        
-#              
+    
+    #p53
+    os.chdir('/Users/yifan/MiSeq/data/MiSeq20190218/R1/p53/fastq')
+    p53=[
+'MI.M03992_0353.001.FLD0001.p53-1_R1.fastq',
+'MI.M03992_0353.001.FLD0002.p53-13_R1.fastq',
+'MI.M03992_0353.001.FLD0003.p53-25_R1.fastq',
+'MI.M03992_0353.001.FLD0009.p53-2_R1.fastq',
+'MI.M03992_0353.001.FLD0010.p53-14_R1.fastq',
+'MI.M03992_0353.001.FLD0017.p53-3_R1.fastq',
+'MI.M03992_0353.001.FLD0018.p53-15_R1.fastq',
+'MI.M03992_0353.001.FLD0025.p53-4_R1.fastq',
+'MI.M03992_0353.001.FLD0026.p53-16_R1.fastq',
+'MI.M03992_0353.001.FLD0033.p53-5_R1.fastq',
+'MI.M03992_0353.001.FLD0034.p53-17_R1.fastq',
+'MI.M03992_0353.001.FLD0041.p53-6_R1.fastq',
+'MI.M03992_0353.001.FLD0042.p53-18_R1.fastq',
+'MI.M03992_0353.001.FLD0049.p53-7_R1.fastq',
+'MI.M03992_0353.001.FLD0050.p53-19_R1.fastq',
+'MI.M03992_0353.001.FLD0057.p53-8_R1.fastq',
+'MI.M03992_0353.001.FLD0058.p53-20_R1.fastq',
+'MI.M03992_0353.001.FLD0065.p53-9_R1.fastq',
+'MI.M03992_0353.001.FLD0066.p53-21_R1.fastq',
+'MI.M03992_0353.001.FLD0073.p53-10_R1.fastq',
+'MI.M03992_0353.001.FLD0074.p53-22_R1.fastq',
+'MI.M03992_0353.001.FLD0081.p53-11_R1.fastq',
+'MI.M03992_0353.001.FLD0082.p53-23_R1.fastq',
+'MI.M03992_0353.001.FLD0089.p53-12_R1.fastq',
+'MI.M03992_0353.001.FLD0090.p53-24_R1.fastq',
+'MI.M03992_0353.001.FLD0328.p53-26_FACs_R1.fastq'
+
+]    
+    for input in p53:
+        writeInput(input,p53_primer)
+        print('This input file has been created: m' + input+'_input.txt')        
+              
    #BRCA1            
     os.chdir('/Users/yifan/MiSeq/data/MiSeq20190218/R1/BRCA1/fastq')
     BRCA1=['MI.M03992_0353.001.FLD0008.BRCA1-10_R1.fastq',
@@ -165,77 +167,80 @@ if __name__=="__main__":
 'MI.M03992_0353.001.FLD0296.BRCA1-22_R1.fastq',
 'MI.M03992_0353.001.FLD0304.BRCA1-23_R1.fastq',
 'MI.M03992_0353.001.FLD0312.BRCA1-24_R1.fastq',
-'MI.M03992_0353.001.FLD0320.BRCA1-25_R1.fastq'
+'MI.M03992_0353.001.FLD0320.BRCA1-25_R1.fastq',
+'MI.M03992_0353.001.FLD0352.BRCA1-26_FACs_R1.fastq'
+
 ]    
     for input in BRCA1:
         writeInput(input,BRCA1_primer)
         print('This input file has been created: m' + input+'_input.txt')    
         
-#     
-#    #PTEN2   
-#    os.chdir('/Users/yifan/MiSeq/data/MiSeq20190218/R1/PTEN2/fastq')
-#    PTEN2=['MI.M03992_0353.001.FLD0004.PTEN2-12_R1.fastq',
-#'MI.M03992_0353.001.FLD0005.PTEN2-24_R1.fastq',
-#'MI.M03992_0353.001.FLD0011.PTEN2-1_R1.fastq',
-#'MI.M03992_0353.001.FLD0012.PTEN2-13_R1.fastq',
-#'MI.M03992_0353.001.FLD0013.PTEN2-25_R1.fastq',
-#'MI.M03992_0353.001.FLD0019.PTEN2-2_R1.fastq',
-#'MI.M03992_0353.001.FLD0020.PTEN2-14_R1.fastq',
-#'MI.M03992_0353.001.FLD0027.PTEN2-3_R1.fastq',
-#'MI.M03992_0353.001.FLD0028.PTEN2-15_R1.fastq',
-#'MI.M03992_0353.001.FLD0035.PTEN2-4_R1.fastq',
-#'MI.M03992_0353.001.FLD0036.PTEN2-16_R1.fastq',
-#'MI.M03992_0353.001.FLD0043.PTEN2-5_R1.fastq',
-#'MI.M03992_0353.001.FLD0044.PTEN2-17_R1.fastq',
-#'MI.M03992_0353.001.FLD0051.PTEN2-6_R1.fastq',
-#'MI.M03992_0353.001.FLD0052.PTEN2-18_R1.fastq',
-#'MI.M03992_0353.001.FLD0059.PTEN2-7_R1.fastq',
-#'MI.M03992_0353.001.FLD0060.PTEN2-19_R1.fastq',
-#'MI.M03992_0353.001.FLD0067.PTEN2-8_R1.fastq',
-#'MI.M03992_0353.001.FLD0068.PTEN2-20_R1.fastq',
-#'MI.M03992_0353.001.FLD0075.PTEN2-9_R1.fastq',
-#'MI.M03992_0353.001.FLD0076.PTEN2-21_R1.fastq',
-#'MI.M03992_0353.001.FLD0083.PTEN2-10_R1.fastq',
-#'MI.M03992_0353.001.FLD0084.PTEN2-22_R1.fastq',
-#'MI.M03992_0353.001.FLD0091.PTEN2-11_R1.fastq',
-#'MI.M03992_0353.001.FLD0092.PTEN2-23_R1.fastq'
-#
-#]    
-#    for input in PTEN2:
-#        writeInput(input,PTEN2_primer)
-#        print('This input file has been created: m' + input+'_input.txt')        
-#    #PTEN3
-#    os.chdir('/Users/yifan/MiSeq/data/MiSeq20190218/R1/PTEN3/fastq')
-#    PTEN3=['MI.M03992_0353.001.FLD0006.PTEN3-11_R1.fastq',
-#'MI.M03992_0353.001.FLD0007.PTEN3-23_R1.fastq',
-#'MI.M03992_0353.001.FLD0014.PTEN3-12_R1.fastq',
-#'MI.M03992_0353.001.FLD0015.PTEN3-24_R1.fastq',
-#'MI.M03992_0353.001.FLD0021.PTEN3-1_R1.fastq',
-#'MI.M03992_0353.001.FLD0022.PTEN3-13_R1.fastq',
-#'MI.M03992_0353.001.FLD0023.PTEN3-25_R1.fastq',
-#'MI.M03992_0353.001.FLD0029.PTEN3-2_R1.fastq',
-#'MI.M03992_0353.001.FLD0030.PTEN3-14_R1.fastq',
-#'MI.M03992_0353.001.FLD0037.PTEN3-3_R1.fastq',
-#'MI.M03992_0353.001.FLD0038.PTEN3-15_R1.fastq',
-#'MI.M03992_0353.001.FLD0045.PTEN3-4_R1.fastq',
-#'MI.M03992_0353.001.FLD0046.PTEN3-16_R1.fastq',
-#'MI.M03992_0353.001.FLD0053.PTEN3-5_R1.fastq',
-#'MI.M03992_0353.001.FLD0054.PTEN3-17_R1.fastq',
-#'MI.M03992_0353.001.FLD0061.PTEN3-6_R1.fastq',
-#'MI.M03992_0353.001.FLD0062.PTEN3-18_R1.fastq',
-#'MI.M03992_0353.001.FLD0069.PTEN3-7_R1.fastq',
-#'MI.M03992_0353.001.FLD0070.PTEN3-19_R1.fastq',
-#'MI.M03992_0353.001.FLD0077.PTEN3-8_R1.fastq',
-#'MI.M03992_0353.001.FLD0078.PTEN3-20_R1.fastq',
-#'MI.M03992_0353.001.FLD0085.PTEN3-9_R1.fastq',
-#'MI.M03992_0353.001.FLD0086.PTEN3-21_R1.fastq',
-#'MI.M03992_0353.001.FLD0093.PTEN3-10_R1.fastq',
-#'MI.M03992_0353.001.FLD0094.PTEN3-22_R1.fastq'
-#
-#]    
-#    for input in PTEN3:
-#        writeInput(input,PTEN3_primer)
-#        print('This input file has been created: m' + input+'_input.txt')
-# 
-#    sys.stdout.close()
-#    sys.stdout=stdoutOrigin
+     
+    #PTEN2   
+    os.chdir('/Users/yifan/MiSeq/data/MiSeq20190218/R1/PTEN2/fastq')
+    PTEN2=['MI.M03992_0353.001.FLD0004.PTEN2-12_R1.fastq',
+'MI.M03992_0353.001.FLD0005.PTEN2-24_R1.fastq',
+'MI.M03992_0353.001.FLD0011.PTEN2-1_R1.fastq',
+'MI.M03992_0353.001.FLD0012.PTEN2-13_R1.fastq',
+'MI.M03992_0353.001.FLD0013.PTEN2-25_R1.fastq',
+'MI.M03992_0353.001.FLD0019.PTEN2-2_R1.fastq',
+'MI.M03992_0353.001.FLD0020.PTEN2-14_R1.fastq',
+'MI.M03992_0353.001.FLD0027.PTEN2-3_R1.fastq',
+'MI.M03992_0353.001.FLD0028.PTEN2-15_R1.fastq',
+'MI.M03992_0353.001.FLD0035.PTEN2-4_R1.fastq',
+'MI.M03992_0353.001.FLD0036.PTEN2-16_R1.fastq',
+'MI.M03992_0353.001.FLD0043.PTEN2-5_R1.fastq',
+'MI.M03992_0353.001.FLD0044.PTEN2-17_R1.fastq',
+'MI.M03992_0353.001.FLD0051.PTEN2-6_R1.fastq',
+'MI.M03992_0353.001.FLD0052.PTEN2-18_R1.fastq',
+'MI.M03992_0353.001.FLD0059.PTEN2-7_R1.fastq',
+'MI.M03992_0353.001.FLD0060.PTEN2-19_R1.fastq',
+'MI.M03992_0353.001.FLD0067.PTEN2-8_R1.fastq',
+'MI.M03992_0353.001.FLD0068.PTEN2-20_R1.fastq',
+'MI.M03992_0353.001.FLD0075.PTEN2-9_R1.fastq',
+'MI.M03992_0353.001.FLD0076.PTEN2-21_R1.fastq',
+'MI.M03992_0353.001.FLD0083.PTEN2-10_R1.fastq',
+'MI.M03992_0353.001.FLD0084.PTEN2-22_R1.fastq',
+'MI.M03992_0353.001.FLD0091.PTEN2-11_R1.fastq',
+'MI.M03992_0353.001.FLD0092.PTEN2-23_R1.fastq',
+'MI.M03992_0353.001.FLD0336.PTEN2-26_FACs_R1.fastq'
+
+]    
+    for input in PTEN2:
+        writeInput(input,PTEN2_primer)
+        print('This input file has been created: m' + input+'_input.txt')        
+    #PTEN3
+    os.chdir('/Users/yifan/MiSeq/data/MiSeq20190218/R1/PTEN3/fastq')
+    PTEN3=['MI.M03992_0353.001.FLD0006.PTEN3-11_R1.fastq',
+'MI.M03992_0353.001.FLD0007.PTEN3-23_R1.fastq',
+'MI.M03992_0353.001.FLD0014.PTEN3-12_R1.fastq',
+'MI.M03992_0353.001.FLD0015.PTEN3-24_R1.fastq',
+'MI.M03992_0353.001.FLD0021.PTEN3-1_R1.fastq',
+'MI.M03992_0353.001.FLD0022.PTEN3-13_R1.fastq',
+'MI.M03992_0353.001.FLD0023.PTEN3-25_R1.fastq',
+'MI.M03992_0353.001.FLD0029.PTEN3-2_R1.fastq',
+'MI.M03992_0353.001.FLD0030.PTEN3-14_R1.fastq',
+'MI.M03992_0353.001.FLD0037.PTEN3-3_R1.fastq',
+'MI.M03992_0353.001.FLD0038.PTEN3-15_R1.fastq',
+'MI.M03992_0353.001.FLD0045.PTEN3-4_R1.fastq',
+'MI.M03992_0353.001.FLD0046.PTEN3-16_R1.fastq',
+'MI.M03992_0353.001.FLD0053.PTEN3-5_R1.fastq',
+'MI.M03992_0353.001.FLD0054.PTEN3-17_R1.fastq',
+'MI.M03992_0353.001.FLD0061.PTEN3-6_R1.fastq',
+'MI.M03992_0353.001.FLD0062.PTEN3-18_R1.fastq',
+'MI.M03992_0353.001.FLD0069.PTEN3-7_R1.fastq',
+'MI.M03992_0353.001.FLD0070.PTEN3-19_R1.fastq',
+'MI.M03992_0353.001.FLD0077.PTEN3-8_R1.fastq',
+'MI.M03992_0353.001.FLD0078.PTEN3-20_R1.fastq',
+'MI.M03992_0353.001.FLD0085.PTEN3-9_R1.fastq',
+'MI.M03992_0353.001.FLD0086.PTEN3-21_R1.fastq',
+'MI.M03992_0353.001.FLD0093.PTEN3-10_R1.fastq',
+'MI.M03992_0353.001.FLD0094.PTEN3-22_R1.fastq',
+'MI.M03992_0353.001.FLD0344.PTEN3-26_FACs_R1.fastq'
+]    
+    for input in PTEN3:
+        writeInput(input,PTEN3_primer)
+        print('This input file has been created: m' + input+'_input.txt')
+ 
+    sys.stdout.close()
+    sys.stdout=stdoutOrigin
